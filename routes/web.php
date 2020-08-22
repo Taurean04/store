0d54590436f96b877e1719da8abd40ee787fd71c
Taurean04 <ayobamitomi@gmail.com>
2020-08-22 08:45:34 +0100

    Admin Model and Migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+require 'admin.php';
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/admin', 'admin.dashboard.index');
